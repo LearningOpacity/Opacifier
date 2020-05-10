34fe12b6c85f2b47a471f8b382ec7b6b8fa26705
Evan Plett <evanplett@gmail.com>
2020-05-10 23:35:30 +0000

    WIP: Adding a couple of more things

--- src/common/Request.js
+++ src/common/Request.js
@@ -1,4 +1,10 @@
+/** Class representing a a request processed by the system */
 export class Request {
+  /**
+   * Create a request
+   * @param {*} requestHost
+   * @param {*} httpRequest
+   */
   constructor(requestHost, httpRequest) {
     this.requestHost = requestHost;
     this.httpRequest = httpRequest;
--- src/coordinator/Coordinator.js
+++ src/coordinator/Coordinator.js
@@ -1,7 +1,11 @@
 require('../common/Request');
 
-
+/** Class that coordinates requests */
 export class Coordinator {
+  /**
+   * Process a request
+   * @param {*} request
+   */
   processRequest(request) {
 
   }
